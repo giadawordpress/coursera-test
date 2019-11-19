3e7fa2184cffc3598de844c994d0af5fb11a5a62
Giada <giada.esempio.com>
2019-11-19 16:13:32 +0100

    hghgh

---
+++ module4-solution/script.js
@@ -0,0 +1,15 @@
+
+// IIFE to create function that displays "Good Bye" + " " + "name" for names that begin with J and 
+// "Hello" + " " + "name" for names that begin with other letters
+
+(function() {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+})();
